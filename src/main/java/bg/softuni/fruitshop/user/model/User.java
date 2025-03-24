3827bd9e3abc4c682443fa050a192adda13ef90f
Petar Georgiev <petar.b.georgiev@gmail.com>
2025-03-24 22:11:29 +0200

    structure

---
+++ src/main/java/bg/softuni/fruitshop/user/model/User.java
@@ -0,0 +1,35 @@
+package bg.softuni.fruitshop.user.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String profilePicture;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
